dee1caf08de44a30a8e02d96e5e031ba53c6abaa
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-29 08:26:18 +0430

    Intent and startActivity

--- app/src/main/java/ir/sadeqsalehi/activitylifecycle/MainActivity.java
+++ app/src/main/java/ir/sadeqsalehi/activitylifecycle/MainActivity.java
@@ -1,7 +1,9 @@
 package ir.sadeqsalehi.activitylifecycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View view) {
+        // Intent for navigate to : activity , settings , sms , call , browser
+        //getApplicationContext() = MainActivity.this
+        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+        startActivity(intent);
     }
 }
