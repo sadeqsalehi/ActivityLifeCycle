b52da08c6564b77fb2e0d0245c6291fa165a4778
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-29 09:07:06 +0430

    Order of Events onCreate , OnStart , onResume , ...

--- app/src/main/java/ir/sadeqsalehi/activitylifecycle/MainActivity.java
+++ app/src/main/java/ir/sadeqsalehi/activitylifecycle/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +12,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "MainActivity : onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "MainActivity : onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "MainActivity : onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "MainActivity : onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "MainActivity : onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "MainActivity : onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "MainActivity : onPause", Toast.LENGTH_SHORT).show();
     }
 
     public void onClickButton(View view) {
         // Intent for navigate to : activity , settings , sms , call , browser
         //getApplicationContext() = MainActivity.this
-        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/ir/sadeqsalehi/activitylifecycle/SecondActivity.java
+++ app/src/main/java/ir/sadeqsalehi/activitylifecycle/SecondActivity.java
@@ -2,6 +2,7 @@ package ir.sadeqsalehi.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +10,41 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Toast.makeText(this, "SecondActivity : onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "SecondActivity : onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "SecondActivity : onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "SecondActivity : onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "SecondActivity : onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "SecondActivity : onStop", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "SecondActivity : onPause", Toast.LENGTH_SHORT).show();
     }
 }
